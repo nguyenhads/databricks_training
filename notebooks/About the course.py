e47d56a13d6acff9d2bd904dd8dc092562da6d50
nguyenha.mse@gmail.com <nguyenha.mse@gmail.com>
2023-06-01 23:26:20 +0000

    update information

--- notebooks/About the course.py
+++ notebooks/About the course.py
@@ -22,6 +22,30 @@
 # MAGIC   * Various data sources to pull from
 # MAGIC   * Lots of different formats to contend with
 # MAGIC   * Time limmited
+# MAGIC
+# MAGIC ## Big Data Needs
+# MAGIC * Collecting - Storing - Using data
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Apache Spark
+# MAGIC * Distributed computing engine
+# MAGIC   * `Data -> Driver -> Distribute to many workers`
+# MAGIC * Work with a variety of data sources and storage formats
+# MAGIC * Accessible from multiple language APIs
+# MAGIC * Characteristic of Spark
+# MAGIC   * Connect to multiple data stores and BI tools
+# MAGIC     * Eg: delta lake, databricks, power bi, sql database, cloud storage, tableau, redash
+# MAGIC   * Accessible from multiple language APIs
+# MAGIC     * python, sql, r, scala
+# MAGIC
+# MAGIC #### Spark SQL
+# MAGIC * Spark library for structured data preprocessing
+# MAGIC * Allows us to use SQL to access Spark
+# MAGIC * Ease of use (easy read and write in many kind of format)
+# MAGIC * Optimized queries
+# MAGIC * Combine with other languages like python, scala, r in a notebook
 
 # COMMAND ----------
 
