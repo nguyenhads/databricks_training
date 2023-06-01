b3835be2d60cf91615fdc2185411bf99e42e0fca
nguyenha.mse@gmail.com <nguyenha.mse@gmail.com>
2023-06-01 23:06:24 +0000

    add new notebook

---
+++ notebooks/About the course (1).py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Overview
+# MAGIC ## Course information
+# MAGIC * Link: [coursera link](https://www.coursera.org/learn/apache-spark-sql-for-data-analysts/home/week/1)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## What is big data
+# MAGIC * The massive of data that is being generated every second of every day
+# MAGIC * 5V
+# MAGIC   * Volume (of data)
+# MAGIC   * Velocity (the speed new data is gnerated and moves)
+# MAGIC   * Variety (different types and cources of data)
+# MAGIC   * Varacity (The quality and accuracy of data)
+# MAGIC   * Value (what is the value from data for the business)
+# MAGIC
+# MAGIC ## Commond struggles with big data
+# MAGIC * Eg: Create a dashboard showing customer transaction data
+# MAGIC   * Hundreds of data files
+# MAGIC   * Various data sources to pull from
+# MAGIC   * Lots of different formats to contend with
+# MAGIC   * Time limmited
+
+# COMMAND ----------
+
+
